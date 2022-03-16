e8e4ba2f37eb9e88fcb250ea51e67835f6d2eef4
Adnan <pozegic.adnan@gmail.com>
2022-03-16 13:02:42 +0100

    Add front-end render for weather

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,40 @@
 console.log( 'Client side JS loaded...' );
+
+
+
+const weatherForm = document.querySelector( 'form' );
+const search      = document.querySelector( 'input' );
+const messageOne  = document.querySelector( '#message-1' );
+const messageTwo  = document.querySelector( '#message-2' );
+
+weatherForm.addEventListener( 'submit', ( event ) => {
+	event.preventDefault();
+
+	messageOne.innerHTML = `<p>Loading...</p>`;
+	messageTwo.innerHTML = '';
+
+	const location = search.value;
+
+	fetch( `/weather?address=${location}` )
+	.then( response => {
+		return response.json()
+	})
+	.then( data => {
+		if ( data.error ) {
+			messageOne.innerHTML = `<p>${data.error}</p>`;
+			return;
+		}
+
+		messageOne.innerHTML = `
+			<h2>Current Weather</h2>
+			<h3>${data.location}</h3>
+			<p>
+				<span><b>Status</b>: ${data.description}</span> <br>
+				<span><b>Temperature</b>: ${data.temperature}°C</span> <br>
+				<span><b>Feels like</b>: ${data.feelsLike}°C</span> <br>
+			</p>
+		`;
+
+		console.log( data );
+	});
+});
