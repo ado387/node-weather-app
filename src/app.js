86afacc840a85b8392e73016533445a45ff2f850
Adnan <pozegic.adnan@gmail.com>
2022-03-16 06:33:50 +0100

    Add routes

--- src/app.js
+++ src/app.js
@@ -6,9 +6,17 @@ app.get( '', ( req, res ) => {
 	res.send( 'Hello express!' );
 });
 
-// app.com
-// app.com/help
-// app.com/about
+app.get( '/help', ( req, res ) => {
+	res.send( 'Help page' );
+});
+
+app.get( '/about', ( req, res ) => {
+	res.send( 'About page' );
+});
+
+app.get( '/weather', ( req, res ) => {
+	res.send( 'Weather' );
+});
 
 app.listen( 3000, () => {
 	console.log( 'Server is up on port 3000.' );
