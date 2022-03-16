2a715192fdd8b70fa6602093641fc24cda49f0c3
Adnan <pozegic.adnan@gmail.com>
2022-03-16 07:37:11 +0100

    Serve static files easily

--- src/app.js
+++ src/app.js
@@ -1,20 +1,17 @@
+const path    = require( 'path' );
 const express = require( 'express' );
 
 const app = express();
 
+const publicDirectoryPath = path.join( __dirname, '../public' );
+const options             = {
+	extensions: [ 'html', 'htm' ],
+}
+
+app.use( express.static( publicDirectoryPath, options ) );
+
 app.get( '', ( req, res ) => {
 	res.send( '<h1>Weather</h1>' );
-});
-
-app.get( '/help', ( req, res ) => {
-	res.send({
-		name: 'Adnan',
-		age: 31,
-	});
-});
-
-app.get( '/about', ( req, res ) => {
-	res.send( '<h1>About page</h1>' );
 });
 
 app.get( '/weather', ( req, res ) => {
