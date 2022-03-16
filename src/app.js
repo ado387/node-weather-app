5144b5cb93adf8e36c0aad2767508e3a13d031e6
Adnan <pozegic.adnan@gmail.com>
2022-03-16 08:50:58 +0100

    Add handlebars templates
    - Config handlebars
    - Delete static files
    - Separate views and partials
    - Use partials in views

--- src/app.js
+++ src/app.js
@@ -1,23 +1,66 @@
 const path    = require( 'path' );
 const express = require( 'express' );
+const hbs     = require( 'hbs' );
 
 const app = express();
 
-const publicDirectoryPath = path.join( __dirname, '../public' );
-const options             = {
+// Define paths for express config.
+const publicPath   = path.join( __dirname, '../public' );
+const viewsPath    = path.join( __dirname, '../templates/views' );
+const partialsPath = path.join( __dirname, '../templates/partials' );
+
+const options    = {
 	extensions: [ 'html', 'htm' ],
 }
 
-app.use( express.static( publicDirectoryPath, options ) );
+// Setup handlebars engine and views location.
+app.set( 'view engine', 'hbs' );
+app.set( 'views', viewsPath );
+hbs.registerPartials( partialsPath );
+
+// Setup static directory to serve.
+app.use( express.static( publicPath, options ) );
 
 app.get( '', ( req, res ) => {
-	res.send( '<h1>Weather</h1>' );
+	res.render( 'index', {
+		title: 'Weather',
+		name: 'Adnan Požegić',
+	});
+});
+
+app.get( '/about', ( req, res ) => {
+	res.render( 'about', {
+		title: 'About',
+		name: 'Adnan Požegić',
+	});
+});
+
+app.get( '/help', ( req, res ) => {
+	res.render( 'help', {
+		title: 'Help',
+		message: 'Here you can find weather app api documentation.',
+		name: 'Adnan Požegić',
+	});
 });
 
 app.get( '/weather', ( req, res ) => {
 	res.send({
 		temperature: 20,
 		location: 'Sarajevo',
+	});
+});
+
+app.get( '/help/*', ( req, res ) => {
+	res.render( '404', {
+		title: '404 Help',
+		name: 'Adnan Požegić',
+	});
+});
+
+app.get( '*', ( req, res ) => {
+	res.render( '404', {
+		title: '404',
+		name: 'Adnan Požegić',
 	});
 });
 
